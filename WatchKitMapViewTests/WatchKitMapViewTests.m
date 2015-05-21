67671ed7a5744815ee3d48209d9c6698ce9cfef1
Than Dang <dtthan@apide.com>
2015-05-21 09:11:47 +0200

    Initial Commit

---
+++ WatchKitMapViewTests/WatchKitMapViewTests.m
@@ -0,0 +1,40 @@
+//
+//  WatchKitMapViewTests.m
+//  WatchKitMapViewTests
+//
+//  Created by Dang Thanh Than on 5/21/15.
+//  Copyright (c) 2015 Than Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface WatchKitMapViewTests : XCTestCase
+
+@end
+
+@implementation WatchKitMapViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
